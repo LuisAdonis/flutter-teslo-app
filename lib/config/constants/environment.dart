798c636e5e6f2325dc8181fbf2f6fea398635f84
Luis Adonis <Paz.Luis@hotmail.es>
2023-08-25 12:30:25 -0500

    sdf

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,14 +1,9 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
-
 class Environment {
-
   static initEnvironment() async {
     await dotenv.load(fileName: '.env');
   }
 
-  static String apiUrl = dotenv.env['API_URL'] ?? 'No está configurado el API_URL';
-
+  static String apiUrl = dotenv.env['api_url'] ?? 'No está configurado el API_URL';
 }
-
